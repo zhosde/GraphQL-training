76873723667ae93cdf6beb305b470f0d6890893d
Hongshan <zhou.hongshan636@gmail.com>
2021-10-01 20:36:47 +0200

    filtering, pagination & sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,27 @@
 // access the prisma object via 'context' argument, allow to access database through Prisma Client API
-function feed(parent, args, context, info) {
-  return context.prisma.link.findMany();
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {}; // if no filter string provided then empty obj
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  })
+
+  const count = await context.prisma.link.count({ where })
+
+  return {
+      links,
+      count,
+  };
 }
 
 module.exports = {
